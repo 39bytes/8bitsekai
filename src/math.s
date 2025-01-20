3a51edfc5545d98cac3d104158ec75305aa22fad
39bytes <jeff@39bytes.dev>
2025-01-20 18:48:21 -0500

    replace math procedures with macros

--- src/math.s
+++ src/math.s
@@ -1,145 +1,159 @@
 ; Math operations do not modify volatile registers so callers
 ; do not have to preserve the registers before calling them.
 
-; 16 bit addition: B + C
-; Preserves: X, Y
-; ---Parameters---
-; p1_16 - B
-; p2_16 - C
-; ---Returns---
-; r1_16 - Result
-.proc add16
+; 16 bit addition 
+.macro ADD16 result, var1, var2
   clc
   ; Add low bytes
-  lda p1_16
-  adc p2_16
-  sta r1_16
+  lda var1
+  adc var2
+  sta result
   ; Add high bytes
-  lda p1_16+1
-  adc p2_16+1
-  sta r1_16+1
+  lda var1+1
+  adc var2+1
+  sta result+1
+.endmacro
 
-  rts
-.endproc
-
-; 24 bit addition: B + C
-; Preserves: X, Y
-; ---Parameters---
-; p1_24 - B
-; p2_24 - C
-; ---Returns---
-; r1_24 - Result
-.proc add24
+; 16 bit addition, with separate bytes for the second argument
+; useful for immediates
+.macro ADD16B result, var1, low, high
   clc
   ; Add low bytes
-  lda p1_24
-  adc p2_24
-  sta r1_24
+  lda var1
+  adc low
+  sta result
+  ; Add high bytes
+  lda var1+1
+  adc high
+  sta result+1
+.endmacro
+
+; 24 bit addition
+.macro ADD24 result, var1, var2
+  clc
+  ; Add low bytes
+  lda var1
+  adc var2
+  sta result
   ; Add middle bytes
-  lda p1_24+1
-  adc p2_24+1
-  sta r1_24+1
+  lda var1+1
+  adc var2+1
+  sta result+1
   ; Add high bytes
-  lda p1_24+2
-  adc p2_24+2
-  sta r1_24+2
+  lda var1+2
+  adc var2+2
+  sta result+2
+.endmacro
 
-  rts
-.endproc
+; 24 bit addition with separate bytes
+.macro ADD24B result, var1, low, middle, high
+  clc
+  ; Add low bytes
+  lda var1
+  adc low
+  sta result
+  ; Add middle bytes
+  lda var1+1
+  adc middle
+  sta result+1
+  ; Add high bytes
+  lda var1+2
+  adc high
+  sta result+2
+.endmacro
 
-; 16 bit subtraction: B - C
-; Preserves: X, Y
-; ---Parameters---
-; p1_16 - B
-; p2_16 - C
-; ---Returns---
-; r1_16 - Result
-.proc sub16
+; 16 bit subtraction
+.macro SUB16 result, var1, var2
   sec
   ; Subtract low bytes
-  lda p1_16
-  sbc p2_16
-  sta r1_16
+  lda var1
+  sbc var2
+  sta result
   ; Subtract borrow from high byte
-  lda p1_16+1
-  sbc p2_16+1
-  sta r1_16+1
+  lda var1+1
+  sbc var2+1
+  sta result+1
+.endmacro
 
-  rts
-.endproc
-
-; 24 bit subtraction: B - C
-; Preserves: X, Y
-; ---Parameters---
-; p1_24 - B
-; p2_24 - C
-; ---Returns---
-; r1_24 - Result
-.proc sub24
+; 16 bit subtraction with separate bytes
+.macro SUB16B result, var1, low, high
   sec
   ; Subtract low bytes
-  lda p1_24
-  sbc p2_24
-  sta r1_24
-  ; Subtract middle bytes
-  lda p1_24+1
-  sbc p2_24+1
-  sta r1_24+1
-  ; Subtract high bytes
-  lda p1_24+2
-  sbc p2_24+2
-  sta r1_24+2
+  lda var1
+  sbc low
+  sta result
+  ; Subtract borrow from high byte
+  lda var1+1
+  sbc high
+  sta result+1
+.endmacro
 
-  rts
+; 24 bit subtraction
+.proc SUB24 result, var1, var2
+  sec
+  ; Subtract low bytes
+  lda var1
+  sbc var2
+  sta result
+  ; Subtract middle bytes
+  lda var1+1
+  sbc var2+1
+  sta result+1
+  ; Subtract high bytes
+  lda var1+2
+  sbc var2+2
+  sta result+2
+.endproc
+
+; 24 bit subtraction with separate bytes
+.proc SUB24B result, var1, low, middle, high
+  sec
+  ; Subtract low bytes
+  lda var1
+  sbc low
+  sta result
+  ; Subtract middle bytes
+  lda var1+1
+  sbc middle
+  sta result+1
+  ; Subtract high bytes
+  lda var1+2
+  sbc high
+  sta result+2
 .endproc
 
 ; 16 bit comparison between two numbers B and C
-; Preserves: X, Y
-; ---Parameters----
-; p1_24 - B
-; p2_24 - C
-; ---Returns---
 ; Sets Z if B == C
 ; Sets C if B >= C
-.proc cmp16
+.macro CMP16 var1, var2
   ; Compare high bytes
-  lda p1_16+1
-  cmp p2_16+1
-  beq :+
-    rts
+  lda var1+1
+  cmp var2+1
+  bne :+ 
+    ; The high bytes are equal, we need to compare the low bytes
+    lda var1
+    cmp var2
 :
-  ; Compare low bytes
-  lda p1_16
-  cmp p2_16
-  rts
-.endproc
+.endmacro
 
 ; 24 bit comparison between two numbers B and C
-; Preserves: X, Y
-; ---Parameters----
-; p1_24 - B
-; p2_24 - C
-; ---Returns---
 ; Sets Z if B == C
 ; Sets C if B >= C
-.proc cmp24
+.macro CMP24 var1, var2
   ; Compare high bytes
-  lda p1_24+2
-  cmp p2_24+2
-  beq :+
-    rts
+  lda var1+2
+  cmp var2+2
+  bne :++
+    ; Compare middle bytes
+    lda var1+1
+    cmp var2+1
+    bne :+
+      ; Compare low bytes
+      lda var1
+      cmp var2
+  :
 :
-  ; Compare middle bytes
-  lda p1_24+1
-  cmp p2_24+1
-  beq :+
-    rts
-:
-  ; Compare low bytes
-  lda p1_24
-  cmp p2_24
-  rts
-.endproc
+.endmacro
 
 ; Convert a byte to an unpacked binary coded decimal representation
 ; ---Parameters---
